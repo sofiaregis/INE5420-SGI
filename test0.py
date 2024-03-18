aa24410886c2e113ad479fdf363106d2b6d46d65
DMeditsch <diego.mmeditsch@gmail.com>
2024-03-17 21:35:49 -0300

    Teste 01

--- test0.py
+++ test0.py
@@ -1,6 +1,29 @@
+#! /usr/bin/env python3
 
-def say_something(smt):
-    print(smt)
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, Gtk, GdkPixbuf
 
-while True:
-    say_something(input())
+
+class WindowMain():
+
+    def __init__(self):
+        # Get GUI Glade file
+        self.builder=Gtk.Builder()
+        self.builder.add_from_file("UI.glade")
+        self.builder.connect_signals(self)
+
+        # Display main window
+        self.windowMain=self.builder.get_object("MainWindow")
+        self.windowMain.show_all()
+
+    def on_window_main_destroy(self, widget, data=None):
+        print("on_window_main_destory")
+        Gtk.main_quit()
+    
+    def main(self):
+        Gtk.main()
+
+if __name__ == "__main__":
+    application=WindowMain()
+    application.main()
