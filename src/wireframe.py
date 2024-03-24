3a4f4c0463b95aaa99714e6ce5e607c2c934c8dc
Sofia <sofia@192.168.1.8>
2024-03-24 13:21:08 -0300

    Added wireframe draw and window navigation functionalities

--- src/wireframe.py
+++ src/wireframe.py
@@ -9,19 +9,19 @@ class Wireframe(GraphicalObject):
         viewport_point_start = viewport.viewport_transformation(self.points[0], window)
         cairo.save()
         cairo.set_source_rgb(0, 0, 0)
-        print("move_to: " + str(viewport_point_start.x) + ", " + str(viewport_point_start.y))
-        cairo.move_to(viewport_point_start.x, viewport_point_start.x)
-        #print("line_to: " + str(viewport_point_start.x) + ", " + str(viewport_point_start.y))
-        #cairo.line_to(viewport_point_start.x, viewport_point_start.x)
-        for point in self.points:
-            viewport_point = viewport.viewport_transformation(point, window)
-            print("line_to: " + str(viewport_point.x) + ", " + str(viewport_point.y))
-            cairo.line_to(viewport_point.x, viewport_point.y)
-        print("line_to: " + str(viewport_point_start.x) + ", " + str(viewport_point_start.y))
-        cairo.line_to(viewport_point_start.x, viewport_point_start.y)
-        cairo.stroke()
+        for i in range(len(self.points)):
+            viewport_point_start = viewport.viewport_transformation(self.points[i], window)
+            if not (i == len(self.points) - 1):
+                viewport_point_end = viewport.viewport_transformation(self.points[i+1], window)
+                cairo.move_to(viewport_point_start.x, viewport_point_start.y)
+                cairo.line_to(viewport_point_end.x, viewport_point_end.y)
+                cairo.stroke()
+            else:
+                viewport_point_end = viewport.viewport_transformation(self.points[0], window)
+                cairo.move_to(viewport_point_start.x, viewport_point_start.y)
+                cairo.line_to(viewport_point_end.x, viewport_point_end.y)
+                cairo.stroke()
         cairo.restore()
-        
 
     def remove():
         pass
