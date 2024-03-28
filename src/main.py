2a7d8a68ad7d05f271ce20f617e416cdf13eb2f7
Sofia <sofia@192.168.1.8>
2024-03-28 09:54:30 -0300

    Fixed bugs in transformator

--- src/main.py
+++ src/main.py
@@ -4,16 +4,22 @@ from point import Point
 from line import Line
 from wireframe import Wireframe
 from window_main import WindowMain
+from transformator import Transformator
 
 world = World()
 viewport = Viewport()
 main_window = WindowMain(world, viewport)
+transformator = Transformator()
 
 #Starting objects in the World for easier visualization
 world.add_object(Wireframe([Point(100,100),Point(200,100),Point(200,200),Point(100,200)]))
 world.add_object(Line(150,300,600,500))
 world.add_object(Point(600,300))
 world.add_object(Wireframe([Point(450,100),Point(350,350),Point(500,200)]))
+
+transformator.move_object(2, world.display_file[0], Point(300,0))
+
+
 
 if __name__ == "__main__":
     main_window.post_init()
